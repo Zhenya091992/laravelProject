f6067e1a507c634c9fb04fae7d9fd425d98b5dd8
”zhenya091992” <zhenya091992@gmail.com>
2022-05-16 14:32:09 +0300

    test

--- public/index.php
+++ public/index.php
@@ -45,7 +45,7 @@ require __DIR__.'/../vendor/autoload.php';
 */
 
 $app = require_once __DIR__.'/../bootstrap/app.php';
-dd($_SESSION);
+dd($_SERVER);
 $kernel = $app->make(Kernel::class);
 
 $response = $kernel->handle(
