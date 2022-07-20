a93fbdac100351e0e204cef3df928afd6e9b5bdb
zhenya091992 <zhenya091992@gmail.com>
2022-07-20 21:49:42 +0300

    add SorceDataTest

--- tests/Feature/SourceDataTest.php
+++ tests/Feature/SourceDataTest.php
@@ -6,9 +6,6 @@ use App\Models\Price;
 use App\Models\SourceData;
 use Faker\Factory;
 use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
-use Illuminate\Support\Carbon;
-use Illuminate\Support\Facades\DB;
 use Tests\TestCase;
 
 class SourceDataTest extends TestCase
