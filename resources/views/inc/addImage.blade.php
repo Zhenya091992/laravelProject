6ba1dc3c05a2fc93ec04ce5f34c934c2829be2b1
zhenya091992 <zhenya091992@gmail.com>
2022-08-09 13:48:51 +0300

    add form for name image

--- resources/views/inc/addImage.blade.php
+++ resources/views/inc/addImage.blade.php
@@ -1,13 +1,16 @@
 @if($errors->any())
-<h4>{{$errors->first()}}</h4>
+<h4 class="error">{{$errors->first()}}</h4>
 @endif
 
 <form action="{{ route('addImagePost',['idSourceData' => $idSourceData]) }}" method="post">
     @csrf
-    <h1 class="h3 mb-3 fw-normal text-white">Paste url address image</h1>
     <div class="mb-3">
         <label for="urlAddressImage">URL address image</label>
         <input name="urlImage" type="url" class="form-control" id="urlAddressImage" placeholder="http//image">
+    </div>
+    <div class="mb-3">
+        <label for="nameImage">Name image</label>
+        <input name="nameImage" type="text" class="form-control" id="nameImage" placeholder="name image">
     </div>
     <button class="w-100 btn btn-lg btn-warning" type="submit">Accept changes</button>
 </form>
